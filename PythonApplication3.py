d5fdcd7cd7f81c734e9e5a31195295b14555b8a2
PedroHOMota <phomota@hotmail.com>
2017-09-12 11:21:59 +0100

    Added answers to q1 and q2

---
+++ PythonApplication3.py
@@ -0,0 +1,11 @@
+import _datetime
+def ProblemSheetQ1(): #Write a program that prints “Hello, world!” to the screen.
+    print("Answer to Q1:\n")
+    print("Hello World\n")
+
+def ProblemSheetQ2(): #Write a program that prints the current time and date to the console.
+    print("Answer to Q2:\n")
+    print(str(_datetime.datetime.today()))
+
+ProblemSheetQ1()
+ProblemSheetQ2()
